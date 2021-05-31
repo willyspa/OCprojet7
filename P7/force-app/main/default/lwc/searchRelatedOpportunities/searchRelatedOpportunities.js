0fb25d9c6c579d191b367f17082a020b967eeee7
willyspa <armed@hotmail.fr>
2021-05-31 19:49:30 +0200

    search component

--- P7/force-app/main/default/lwc/searchRelatedOpportunities/searchRelatedOpportunities.js
+++ P7/force-app/main/default/lwc/searchRelatedOpportunities/searchRelatedOpportunities.js
@@ -1,7 +1,31 @@
 import { api, LightningElement } from 'lwc';
-import searchedOpportunities from '@salesforce/apex/AccountController.getOpportunities';
-import { RecordFieldDataType } from 'lightning/uiRecordApi';
+import searchOpportunities from '@salesforce/apex/AccountController.getOpportunities';
+
+const columns = [
+    { label: 'Name', fieldName: 'Name',type: 'link' },
+    { label: 'Stage', fieldName: 'StageName'},
+    { label: 'Amount', fieldName: 'Amount', type: 'date', type: 'currency' },
+    { label: 'Close Date', fieldName: 'CloseDate',type: 'date' } 
+];
 
 export default class SearchRelatedOpportunities extends LightningElement {
+
+    opps;
+    searchedOpportunity='';
+    error;
+    columns = columns;
+
     @api recordId;
+ 
+    handleKey(event){
+        this.searchedOpportunity = event.target.value;        
+    }
+    handleSearchOpportunity(){
+        searchOpportunities({"RelatedAccount":this.recordId,"searchedOpportunities":this.searchedOpportunity})
+        .then(result =>{this.opps = result})
+        .catch(error=>{this.error = error})
+        console.log(this.opps);
+    }
+    
+
 }
