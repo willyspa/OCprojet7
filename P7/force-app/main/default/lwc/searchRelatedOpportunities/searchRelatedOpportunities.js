90fb1f11b01d655dc8840e56f9a28754f5555b72
willyspa <armed@hotmail.fr>
2021-06-02 01:09:22 +0200

    Link added to the opportunities list

--- P7/force-app/main/default/lwc/searchRelatedOpportunities/searchRelatedOpportunities.js
+++ P7/force-app/main/default/lwc/searchRelatedOpportunities/searchRelatedOpportunities.js
@@ -2,7 +2,11 @@ import { api, LightningElement } from 'lwc';
 import searchOpportunities from '@salesforce/apex/AccountController.getOpportunities';
 
 const columns = [
-    { label: 'Name', fieldName: 'Name',type: 'link' },
+    { label: 'Name', fieldName: 'NameUrl',type: 'url',
+        typeAttributes:{
+            label:{fieldName:'Name'},target:'_blank'
+        }
+    },
     { label: 'Stage', fieldName: 'StageName'},
     { label: 'Amount', fieldName: 'Amount', type: 'date', type: 'currency' },
     { label: 'Close Date', fieldName: 'CloseDate',type: 'date' } 
@@ -16,15 +20,26 @@ export default class SearchRelatedOpportunities extends LightningElement {
     columns = columns;
 
     @api recordId;
- 
+    //on change the keyword is stored in a variable
     handleKey(event){
         this.searchedOpportunity = event.target.value;        
     }
+    //when the button is clicked the apex controller is called with two attributes
     handleSearchOpportunity(){
         searchOpportunities({"RelatedAccount":this.recordId,"searchedOpportunities":this.searchedOpportunity})
-        .then(result =>{this.opps = result})
+        .then(data =>{
+            //clone of the opportunity list received from the apex controller
+            let clonedData=JSON.parse(JSON.stringify(data));
+            let baseUrl = 'https://'+location.host+'/';
+            //construct the url for each record within the list 
+            clonedData.forEach(opp => {
+                opp.NameUrl = baseUrl+opp.Id;
+            });
+            console.log(data);
+            this.opps = clonedData;
+        })
         .catch(error=>{this.error = error})
-        console.log(this.opps);
+        this.opps = undefined;
     }
     
 
