ba1adb7cf4d3c6d4283842f6cc3cdc9578f2c567
willyspa <armed@hotmail.fr>
2021-06-21 19:48:22 +0200

    last change

--- P7/force-app/main/default/aura/updateLead/updateLeadController.js
+++ P7/force-app/main/default/aura/updateLead/updateLeadController.js
@@ -7,9 +7,11 @@
         console.log(component.get("v.recordId"));
         // check the promise and refresh
         action.setCallback(this, function(response){
+
             var dismissActionPanel = $A.get("e.force:closeQuickAction");
             dismissActionPanel.fire();
             let state = response.getState();
+            
             if (state === "SUCCESS") {
                 console.log("success");
                 $A.get('e.force:refreshView').fire();
